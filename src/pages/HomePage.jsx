9164eab6460cc77fe7bdbe8ef0be590784c89698
Leiladany <leiladany16@gmail.com>
2024-07-18 16:46:06 +0100

    add input

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import {Input, Stack} from '@mui/joy';
+
+
+export const HomePage = () => {
+
+    return (
+
+        <Stack>
+            <Input placeholder="Type in here…" />
+        </Stack>
+    );
+};
